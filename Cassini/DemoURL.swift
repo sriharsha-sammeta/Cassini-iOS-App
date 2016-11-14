5e2778d30dc95483094bcf9a2c5054dfe1d0beeb
sriharsha sammeta <sriharsha4444@icloud.com>
2016-11-14 13:40:32 +0530

    Final Version

---
+++ Cassini/DemoURL.swift
@@ -0,0 +1,22 @@
+//
+//  DemoURL.swift
+//  Cassini
+//
+//  Created by Sriharshaa Sammeta on 2/20/15.
+//  Copyright (c) 2015 Sriharshaa Sammeta. All rights reserved.
+//
+
+import Foundation
+
+struct DemoURL
+{
+    static let stanford = NSURL(string: "http://comm.stanford.edu/wp-content/uploads/2013/01/stanford-campus.png")
+    struct NASA {
+        static let Cassini = NSURL(string: "https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/1526866_10201926287617815_1794608105_n.jpg?oh=b0ef264171e7f4c01e4661640f062655&oe=559518FC&__gda__=1431547171_bde59b0ee1db1a3c5b96af14b0f9d1ed")
+//        http://www.jpl.nasa.gov/images/cassini/20090202/pia03883-full.jpg
+        static let Earth = NSURL(string: "https://scontent-hkg.xx.fbcdn.net/hphotos-prn2/v/t1.0-9/541825_4116958161282_1875092401_n.jpg?oh=177ad06c6a5d0fbc99f50c8b42ffaedb&oe=55958424")
+//        http://www.nasa.gov/sites/default/files/wave_earth_mosaic_3.jpg
+        static let Saturn = NSURL(string: "https://scontent-hkg.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/10931421_10204585566298120_8007284752440727210_n.jpg?oh=826b8e9558995ca0f99a9feeced8f35d&oe=55826F07")
+//        http://www.nasa.gov/sites/default/files/saturn_collage.jpg
+    }
+}
--- Cassini/ViewController.swift
+++ Cassini/ViewController.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController
+{
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let ivc = segue.destinationViewController as? ImageViewController{
+        if let identifier = segue.identifier{
+            switch identifier{
+                
+                case "Earth":
+                    ivc.imageURL = DemoURL.NASA.Earth
+                    ivc.title = "Earth"
+                case "Cassini":
+                    ivc.imageURL = DemoURL.NASA.Cassini
+                    ivc.title = "Cassini"
+                case "Saturn":
+                    ivc.imageURL = DemoURL.NASA.Saturn
+                    ivc.title = "Saturn"
+                default : break
+            }
+        
+        }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
